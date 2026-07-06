82d1460b4946c47a2521fccc1cfa743bc9e9c449
Wendy Barcelon <wendybarcelon5@gmail.com>
2026-07-06 10:44:24 +0800

    Debug env vars

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
+console.log("URL exists:", !!process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("KEY exists:", !!process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY);
+
 export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
