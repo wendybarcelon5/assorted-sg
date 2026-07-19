0120e979601f2c64c4d176fc157cc610b7b582f7
Wendy Barcelon <wendybarcelon5@gmail.com>
2026-07-19 13:20:15 +0800

    Create My Reviews page

---
+++ app/my-reviews/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export default function MyReviewsPage() {
+  return (
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-5xl font-black">
+          My Reviews
+        </h1>
+
+        <p className="mt-4 text-gray-400">
+          Your product reviews will appear here.
+        </p>
+      </div>
+    </main>
+  );
+}
